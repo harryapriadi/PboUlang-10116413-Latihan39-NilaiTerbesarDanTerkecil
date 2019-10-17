4d027b7102fd9b7a4dedcb6daae120d4ce70aaf4
Harry Apriadi <harryapriadi1@gmail.com>
2019-10-17 14:21:25 +0700

    PboUlang-10116413-Latihan39-NilaiTerbesarDanTerkecil

---
+++ src/pboulang/pkg10116413/latihan39/nilaiterbesardanterkecil/Petugas.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulang.pkg10116413.latihan39.nilaiterbesardanterkecil;
+import java.util.Scanner;
+/**
+ *
+ * @author Lenovo
+ */
+public class Petugas {
+ public String namaPetugas;
+    DaftarNilai dafNilai = new DaftarNilai();
+    Scanner scn = new Scanner(System.in);
+
+    public void inputPetugas() {
+        System.out.print("Masukkan Nama Petugas : ");
+        namaPetugas = scn.next();
+    }
+}
